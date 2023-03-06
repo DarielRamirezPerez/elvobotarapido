f291f3db0a637c117154275769141a2fa263ec8d
DarielRamirezPerez <103982256+DarielRamirezPerez@users.noreply.github.com>
2023-03-06 12:29:21 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 api_hash = "20c1618672d0d23844bf79d8a25de44f"
 api_id = 24253609
 bot_token = "5677942783:AAHY1KH8LA6V9iYTmeUC1UipJd47d-vZDKg"
-bot_admin_group = -1001774085741
+bot_admin_group = darielxd
 
